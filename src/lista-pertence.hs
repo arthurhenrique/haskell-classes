e77f9b6653652fc53440e41bbe708b781baef6d0
Arthur Henrique <arthur393@gmail.com>
2016-08-09 20:35:03 -0300

    changed

--- src/lista-pertence.hs
+++ src/lista-pertence.hs
@@ -1,4 +1,4 @@
 pertence :: [Int] -> Int -> Bool
 pertence [] _ = False
 pertence (x:xs) n   | ( x == n ) = True
-                    | otherwise pertence xs n
+                    | otherwise  = pertence xs n
