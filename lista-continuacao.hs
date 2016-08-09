1cb1bf70c86a81dd4ef9a74e97788865d53cf619
Arthur Henrique <arthur393@gmail.com>
2016-08-09 20:31:33 -0300

    deleted

--- lista-continuacao.hs
+++
@@ -1,7 +0,0 @@
--- Adicionar elementos na lista
--- "arthur":["Henrique","Silva"]
-
--- Coprimento de uma lista
-
-size_list [] = 0
-size_list (x:xs) = 1 + size_list xs
--- nome.hs
+++
@@ -1,7 +0,0 @@
---tuplas
-nomes :: (String, String, String)
-nomes = ("Arthur", "Henrique", "Silva") 
-
-selec_pri(x,_,_) = x
-selec_sec(_,x,_) = x
-selec_ter(_,_,x) = x
