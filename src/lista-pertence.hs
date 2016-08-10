75abd1c0a697a58751461070f79d1cc08a3b3914
Arthur Henrique <arthur393@gmail.com>
2016-08-09 21:26:40 -0300

    add function de pares

--- src/lista-pertence.hs
+++ src/lista-pertence.hs
@@ -2,3 +2,15 @@ pertence :: [Int] -> Int -> Bool
 pertence [] _ = False
 pertence (x:xs) n   | ( x == n ) = True
                     | otherwise  = pertence xs n
+
+
+maior :: [Integer] -> Integer
+maior [x] = x
+maior (x:xs)    | ( x > maior xs ) = x
+                | otherwise = maior xs 
+
+
+is_pairs :: [Int] -> Bool
+is_pairs [] = True
+is_pairs (x:xs) | ( x `mod` 2 == 0 ) = is_pairs xs
+                | otherwise = False
