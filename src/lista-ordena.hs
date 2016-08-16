7657ab7cebe59fd6efc206538d337dafbba59fcf
Arthur Henrique Da Silva <ahsilva2@stefanini.com>
2016-08-16 09:34:35 -0300

    ordena

--- src/lista-ordena.hs
+++ src/lista-ordena.hs
@@ -1,6 +1,9 @@
+lista :: [Int]
+lista = [1,6,2,5,3,4]
+
 menor :: [Int] -> Int
 menor []  = error "[]"
-menor [x] = x
+menor [x] = x 
 menor (x:xs) | (x < menor xs) = x
              | otherwise      = menor xs
 
@@ -9,5 +12,11 @@ removeMenor [] = []
 removeMenor (x:xs)  | (x == menor (x:xs)) = xs
                     | otherwise = (x: removeMenor xs)
 
+ordenaTmp :: [Int] -> [Int] -> [Int]
+ordenaTmp listaOrdenada [] = listaOrdenada
+ordenaTmp listaOrdenada (x:xs) = ordenaTmp(listaOrdenada++[menor(x:xs)]) 
+                                          (removeMenor (x:xs))
 
-
+ordena :: [Int] -> [Int]
+ordena [] = []
+ordena lista = ordenaTmp [] lista
